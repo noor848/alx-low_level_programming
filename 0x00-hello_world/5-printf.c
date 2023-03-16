b717a8cc1f07c7c549acab68598a2a49214a45b8
Noor <noor.braik@hotmail.com>
2023-03-16 13:55:34 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- * Description: A C program that prints with put function
+ * Description: A C program that prints with printf
  * Return: 0---> Success
 */
 
 int main(void)
 {
-	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

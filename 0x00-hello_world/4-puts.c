6500e98959ddc7bb4ddbd241632f65fe022bf3d2
Noor <noor.braik@hotmail.com>
2023-03-16 13:49:42 -0700

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
  * main - Entry point
  * Description: A C program that prints with put function
  * Return: 0---> Success
 */
 
-
-
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }

f755957926b32f7aed01b52b3aa36092300e8f4e
Noor <noor.braik@hotmail.com>
2023-03-17 16:21:26 -0700

    tas5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+int main(void)
+{
+	char c = 'a';
+
+	while(c <= 'z')
+	{
+		if(c != 'q' || c != 'e')
+			putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

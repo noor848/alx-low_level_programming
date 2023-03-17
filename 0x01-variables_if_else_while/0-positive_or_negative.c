84e19faf243720bf1581a516c3f9dcd35a820a84
Noor <noor.braik@hotmail.com>
2023-03-17 14:48:02 -0700

    create readme file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Description: Print The Status of n Value ---> Positive OR Negative
+ * Return: Code 0 is translated as Success!
+*/
 
 int main(void)
 {
@@ -11,12 +15,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
+	/*Code*/
 	if (n > 0)
-	    printf("%i is positive\n", n);
-	else if (n < 0)
-	    printf("%i is negative\n", n);
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
 	else
-	    printf("%i is zero\n", n);
+		printf("%i is negative\n", n);
+
 	return (0);
 }

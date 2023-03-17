021a2173418b05ef4458d3d91bf7a9c1e9fa27d4
Noor <noor.braik@hotmail.com>
2023-03-17 15:51:06 -0700

    task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char n = 'a';
+
+	/* your code goes there */
+	while (n < 'z')
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

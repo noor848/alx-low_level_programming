74cfec27670f5d02ecc4fe123fc4735ff7e30ab4
Noor <noor.braik@hotmail.com>
2023-03-17 17:42:14 -0700

    k

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print all possible all possible of two-digits using putchar
+ * Return: Code 0 is translated as Success!
+*/
+
+int main(void)
+{
+	int firstDigit = 0, seconDigit;
+
+	while (firstDigit <= 99)
+	{
+		seconDigit = firstDigit;
+		while (seconDigit <= 99)
+		{
+			if (seconDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((seconDigit / 10) + 48);
+				putchar((seconDigit % 10) + 48);
+
+				if (firstDigit != 98 || seconDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++seconDigit;
+		}
+		++firstDigit;
+	}
+	putchar('\n');
+
+	return (0);
+}

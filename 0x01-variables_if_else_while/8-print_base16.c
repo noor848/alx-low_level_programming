788dd8649b9993ad8e401f9ad1b54746289f0d64
Noor <noor.braik@hotmail.com>
2023-03-17 17:05:51 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,18 +8,15 @@
 
 int main(void)
 {
-	int num = 0, k = 0;
-	
+	int num = 0, k = 0;	
 	while (num<=9)
 	{
 		putchar(num + 48);
 		num++;
 	}
-	
-	while (k <= 6)
+	while (k < 6)
 	{
 		putchar(num - 1 + 48 + 40 + k);
-	
 		k++;
 	}
 	putchar('\n');

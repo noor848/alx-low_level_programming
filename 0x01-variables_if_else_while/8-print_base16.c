9bee9bf6bf020beb0dd73beb0b610d09ad985e78
Noor <noor.braik@hotmail.com>
2023-03-17 17:03:00 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: hex values 0-F
+ * Return: Code 0 is translated as Success!
+*/
+
+int main(void)
+{
+	int num = 0, k = 0;
+	
+	while (num<=9)
+	{
+		putchar(num + 48);
+		num++;
+	}
+	
+	while(k <= 6)
+	{
+		putchar(num-1+48+40+k);
+	
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+

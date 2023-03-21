f919d43b2907b9c634e373539338cd951b960a34
Noor <noor.braik@hotmail.com>
2023-03-21 13:06:31 -0700

    printalphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ */
+
+void print_alphabet(void)
+{
+	char cha = 'a';
+	
+	while (cha <= 'z')
+	{
+		_putchar(cha);
+		cha++;
+	}
+	
+	_putchar('\n');
+}

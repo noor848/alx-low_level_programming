4e08db8ff2a95c0d95988d7adffd9c9c5c81971a
Noor <noor.braik@hotmail.com>
2023-03-22 13:40:55 -0700

    hange content of the file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - check for multiples of 3 and 5
- *
- * Return: 0 always
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower
+ * Return : Always return 0
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	int x = 1024, y, sum = 0;
+	char ch;
+	int i;
 
-	for (y = 0; y < x; y++)
+	i = 0;
+
+	while (i < 10)
 	{
-		if ((y % 3 == 0) || (y % 5 == 0))
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			sum = sum + y;
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
 	}
-	printf("%d\n", sum);
-	return (0);
 }

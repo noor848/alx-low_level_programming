09eda39676c5fcdc98835f6433e219d673830ad9
Noor <noor.braik@hotmail.com>
2023-04-05 15:39:27 -0700

    t2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints count of string
+ *
+ * @s: the string
+ *
+ * Return: count
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	
+	return _strlen_recursion(s + 1)++;
+	
+}

6211c24551b2fd79c2f34c0ed649ed5a6bb8d51d
noor848 <54281955+noor848@users.noreply.github.com>
2023-04-06 01:22:44 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - a function that prints reverse string
+ * _puts_recursion - a function that prints a new line
  *
  * @s: the string
  *
@@ -10,13 +10,15 @@
 
 
 
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
+		
 	}
 	
+	_putchar(*s);
 	_puts_recursion(s+1);
 }
